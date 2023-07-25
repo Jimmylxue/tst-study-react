7ff46dc3eb2c564e50ee5fa238f1a5fa47c6ab2b
Yionse <2458015575@qq.com>
2023-07-25 13:33:11 +0800

    :bug: Antd-组件显示异常

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,87 @@
 // 引入css
 import "./App.css";
 // 引入mobx模块，让数据响应式
-import { observer } from 'mobx-react-lite';
+import { observer } from "mobx-react-lite";
 // 引入store
-import useStore from './Store/index';
+import useStore from "./Store/index";
+// 引如antd中的组件
+import { Button, Space, Modal } from "antd";
+// 引入React方法
+import { useState } from "react";
 
-const  Row = observer(({row, rowIndex} : any)=> {	
-	/**
-	 * Row代表着每一行的数据，需要将row便利出来
-	 * @param rowIndex 每行的行索引，一共三行
-	 * @param row 每行的数据
-	 */
-	// 获取实例并解构
-	const { chessControl } = useStore();
+const Row = observer(({ row, rowIndex }: any) => {
+  /**
+   * Row代表着每一行的数据，需要将row便利出来
+   * @param rowIndex 每行的行索引，一共三行
+   * @param row 每行的数据
+   */
+  // 获取实例并解构
+  const { chessControl } = useStore();
 
-	// 下子操作
-	function handleClick( cluIndex : number ) {
-		// clu列索引
-		chessControl.chessClick(rowIndex, cluIndex);
-	}
+  // 下子操作
+  function handleClick(cluIndex: number) {
+    // clu列索引
+    chessControl.chessClick(rowIndex, cluIndex);
+  }
 
-	return (
-		<div className='row'>
-			{row.map((item, index) => {
-				return (
-						<button 
-							key={index}
-							onClick={ () => {
-								handleClick(index);
-							} }
-						>
-						{
-							item === 0 ? '-' : (item === 1 ? 'O' : 'X')
-						}</button>
-					);
-			})}
-		</div>
-	);
-})
+  return (
+    <div className="row">
+      {row.map((item, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => {
+              handleClick(index);
+            }}
+          >
+            {item === 0 ? "-" : item === 1 ? "O" : "X"}
+          </button>
+        );
+      })}
+    </div>
+  );
+});
 
 function App() {
-	// 获取实例并解构
-	const { chessControl } = useStore();
-	return (
-		<div className='chess'>
-			{chessControl.chess.map((item, index) => {
-				// 将当前这一行的数据传过去，然后还需要将当前的行索引传过去
-				return <Row key={index} row={item} rowIndex = {index}/>
-			})
-			}
-		</div>
-	);
+  // 获取实例并解构
+  const { chessControl } = useStore();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsModalOpen(true);
+  };
+
+	const handleOk = () => {
+		console.log('确认');
+		setIsModalOpen(false);
+	}
+
+	const handleCancel = () => {
+		console.log('取消');
+		setIsModalOpen(false);
+	}
+
+  return (
+    <>
+      <div className="chess">
+        {chessControl.chess.map((item, index) => {
+          // 将当前这一行的数据传过去，然后还需要将当前的行索引传过去
+          return <Row key={index} row={item} rowIndex={index} />;
+        })}
+      </div>
+      <Button type="primary" onClick={handleClick}>
+        重新开始
+      </Button>
+      <Modal
+        title="提醒"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>确定要重新开始吗</p>
+      </Modal>
+    </>
+  );
 }
 
 export default observer(App);
--- src/Store/ChessControl.tsx
+++ src/Store/ChessControl.tsx
@@ -9,144 +9,143 @@ export default class ChessControl {
    * 1代表一方，-1代表另一方
    * 更换为五子棋的话，扩容数组即可，然后将棋盘的长宽分别修改
    */
-  chess : number[][] = [
-    [0, 0, 0], 
-    [0, 0, 0], 
-    [0, 0, 0], 
+  chess: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ];
   // active-当前应该下的棋子
-  active : number = 1;
+  active: number = 1;
 
   constructor() {
     makeAutoObservable(this);
   }
 
   // 落子操作
-  chessClick = (rowIndex : number, cluIndex : number ) => {
+  chessClick = (rowIndex: number, cluIndex: number) => {
     if (this.chess[rowIndex][cluIndex] === 0) {
       // 将当前应该下的子下在这个地方
       this.chess[rowIndex][cluIndex] = this.active;
       // 切换下一步
       this.active = this.active === 1 ? -1 : 1;
     }
-    this.checkWin(rowIndex , cluIndex);
-  }
+    this.checkWin();
+  };
 
-  // 落子后，检查各个方向的棋子是否达到赢的标准
-  checkWin(rowIndex : number, cluIndex : number) {
+  // 落子后，逐个检查各个方向的棋子是否达到赢的标准
+  checkWin() {
     /**
      * 当落子后，就需要检查当前游戏是否已经结束
      * 判断井字棋的方法就是看横竖与交叉是否存在连续三个同方棋子
      */
     // 先检查当前落子的这个位置的横纵是否存在连续三颗棋子
-    // 行
-    let count : number = 0;   //  计数，如果为3则当前游戏结束
-    const currentChess : number = this.chess[rowIndex as number][cluIndex]; //  当前的棋子
-    const chessLength : number = this.chess[rowIndex as number].length;   //  当前是几阶，可推广到五子棋
-    // 横
-    for ( let i = 0; i < chessLength; i++) {
-      if (currentChess === this.chess[rowIndex as number][i]) {
-        count++;
-      }else {
-        // 如果有一个棋子不一样，直接跳出循环
-        break;
+    let isWin: boolean = false; // 是否已经赢了
+
+    for (let i = 0; i < this.chess.length; i++) {
+      for (let j = 0; j < this.chess[i].length; j++) {
+        // 逐个便利所有的棋子，这里可以封装一个方法区使用
+        if (this.chess[i][j] !== 0) {
+          isWin = this.counter(i, j);
+          if (isWin) {
+            // 如果赢了，则重新开始
+            alert("游戏结束");
+            this.chess = [
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+            ];
+            return;
+          }
+        }
       }
     }
-    if (!this.isFinish(count, chessLength)) {
-      count = 0;
-    }
+  }
 
-    // 竖
-    for (let i = 0; i < chessLength; i++) {
-      if (currentChess === this.chess[i][cluIndex]) {
+  // 数棋子
+  counter(row: number, clu: number): boolean {
+    // 右侧方向
+    const currentChess: number = this.chess[row][clu];
+    let count: number = 1; // 当前方向的棋子有多少
+    for (let i = 1; i < 5; i++) {
+      if (clu + i < 8 && currentChess === this.chess[row][clu + i]) {
+        //  判断是否出现索引越界
         count++;
-      }else {
-        break;
+      } else {
+        break; //  当数组下标越界或者是出现了不匹配的棋子，那么就退出此次循环
       }
     }
-    if (!this.isFinish(count, chessLength)) {
-      count = 0;
-    }
-    
-    let myRow : number = rowIndex - 1;
-    let myClu : number = cluIndex - 1;
+    if (count === 5) return true;
 
-    // 由于是直接从左上开始，需要将当前位置的棋子算进去，所以置count为1
-    count = 1;
-    
-    // 交叉,左上方向
-    while ( myRow >= 0 && myClu>= 0 ) {
-      if (this.chess[myRow][myClu] === currentChess) {  //  如果相等
+    // 右下方向
+    count = 1; //  重置count的值
+    for (let i = 1; i < 5; i++) {
+      if (
+        row + i < 8 &&
+        clu + i < 8 &&
+        currentChess === this.chess[row + i][clu + i]
+      ) {
+        //  判断是否出现索引越界
         count++;
-      }else {
-        break;
+      } else {
+        break; //  当数组下标越界或者是出现了不匹配的棋子，那么就退出此次循环
       }
-      myRow--;
-      myClu--;
     }
+    if (count === 5) return true;
 
-    
-    // 右下
-    myRow = rowIndex + 1;
-    myClu = cluIndex + 1;
-    while ( myRow < chessLength && myClu < chessLength ) {
-      if (this.chess[myRow][myClu] === currentChess) {  //  如果相等
+    // 下侧方向
+    count = 1; //  重置count的值
+    for (let i = 1; i < 5; i++) {
+      if (row + i < 8 && currentChess === this.chess[row + i][clu]) {
+        //  判断是否出现索引越界
         count++;
-      }else {
-        break;
+      } else {
+        break; //  当数组下标越界或者是出现了不匹配的棋子，那么就退出此次循环
       }
-      myRow++;
-      myClu++;
     }
-    
-    if (!this.isFinish(count, chessLength)) {
-      count = 0;
-    }
+    if (count === 5) return true;
 
-    // 交叉右上
-    myRow = rowIndex - 1;
-    myClu = cluIndex + 1;
-    count = 1;
-    while (myRow >= 0 && myClu < chessLength) {
-      if (this.chess[myRow][myClu] === currentChess) {  //  如果相等
+    // 左下方向
+    count = 1; //  重置count的值
+    for (let i = 1; i < 5; i++) {
+      if (row + i < 8 && clu - i >= 0 && this.chess[row + i][clu - i]) {
+        //  判断是否出现索引越界
         count++;
-      }else {
-        break;
+      } else {
+        break; //  当数组下标越界或者是出现了不匹配的棋子，那么就退出此次循环
       }
-      myRow--;
-      myClu++;
     }
+    if (count === 5) return true;
 
-    myRow = rowIndex + 1;
-    myClu = cluIndex - 1;
-    while (myRow < chessLength && myClu >= 0) {
-      if (this.chess[myRow][myClu] === currentChess) {  //  如果相等
-        count++;
-      }else {
-        break;
-      }
-      myRow++;
-      myClu--;
-    }
-
-    if (!this.isFinish(count, chessLength)) {
-      count = 0;
-    }
+    return false;
   }
 
   // 是否游戏结束
-  isFinish( count : number, chessLength : number ) : any {
+  isFinish(count: number, chessLength: number): any {
     if (count === chessLength) {
-      alert('游戏结束');
+      alert("游戏结束");
       // 然后重新开始，重置数组
       this.chess = [
-        [0, 0, 0], 
-        [0, 0, 0], 
-        [0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
       ];
-    }else {
+    } else {
       return false;
     }
   }
-  
 }
